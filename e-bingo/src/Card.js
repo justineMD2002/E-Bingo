4e99127c9b19fce034777e5a140c4c8b7383289a
Justine M. Daugdaug <daugdaug.justine.m@gmail.com>
2024-03-06 22:13:16 +0800

    fixed card tokens bug

--- e-bingo/src/Card.js
+++ e-bingo/src/Card.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 const Card = () => {
   const [cards, setCards] = useState([]); 
   const [code, setCode] = useState(''); 
-  const [token, setToken] = useState('');
   const [arrCode, setArrCode] = useState([]);
   const [error, setError] = useState(''); 
 
@@ -27,29 +26,31 @@ const Card = () => {
     const interval = setInterval(() => {
       for(const playcardToken of arrCode) {
         getWin(playcardToken);
-      }
+      } 
     }, 1000); 
     return () => clearInterval(interval);
   }, [arrCode]); 
 
   const getData = async () => {
-      try {
-        const response = await axios.get(`http://www.hyeumine.com/getcard.php?bcode=${code}`);
-        if(response.data === 0) { 
-          setError('Game code not found. Please try a different code.');
-          return; 
-        }
-        setError('');
-        const card = response.data.card;
-        const numbersArray = [card.B, card.I, card.N, card.G, card.O];
-        const transposedArray = transpose(numbersArray);
-        setCards(prevCards => [...prevCards, transposedArray]);
-        setToken(response.data.playcard_token);
-        setArrCode(prevArrCode => [...prevArrCode, response.data.playcard_token]);
-      } catch (err) {
-          console.error(err);
+    try {
+      const response = await axios.get(`http://www.hyeumine.com/getcard.php?bcode=${code}`);
+      if(response.data === 0) { 
+        setError('Game code not found. Please try a different code.');
+        return; 
       }
-  };
+      setError('');
+      const card = response.data.card;
+      const numbersArray = [card.B, card.I, card.N, card.G, card.O];
+      const transposedArray = transpose(numbersArray);
+      const playcardToken = response.data.playcard_token;
+      
+      setCards(prevCards => [...prevCards, { numbers: transposedArray, token: playcardToken }]);
+      setArrCode(prevArrCode => [...prevArrCode, playcardToken]);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
 
   const transpose = (array) => {  
     return array[0].map((_, colIndex) => array.map(row => row[colIndex]));
@@ -65,7 +66,7 @@ const Card = () => {
 
   const handleSubmitCode = (e) => {
     e.preventDefault(); 
-    alert("Game Joined")
+    getData();
   };
 
   return (
@@ -84,25 +85,25 @@ const Card = () => {
       <h1>Game Code: {code}</h1>
       <button onClick={handleOnClick}>Add Card</button>
       <div className="card">
-        {cards.map((numbers, cardIndex) => (
-              <div key={cardIndex} className="bingo-card"> 
-                <div className="bingo-header">
-                  {['B', 'I', 'N', 'G', 'O'].map((letter, index) => (
-                    <div key={index} className="bingo-header-cell">{letter}</div>
-                  ))}
-                </div>
-                {numbers.map((column, columnIndex) => (
-                  <div key={columnIndex} className="bingo-column">
-                    {column.map((number, numberIndex) => (
-                      <div key={numberIndex} className="bingo-cell">
-                        {number}
-                      </div>
-                    ))}
+        {cards.map((card, cardIndex) => (
+          <div key={cardIndex} className="bingo-card"> 
+            <div className="bingo-header">
+              {['B', 'I', 'N', 'G', 'O'].map((letter, index) => (
+                <div key={index} className="bingo-header-cell">{letter}</div>
+              ))}
+            </div>
+            {card.numbers.map((column, columnIndex) => (
+              <div key={columnIndex} className="bingo-column">
+                {column.map((number, numberIndex) => (
+                  <div key={numberIndex} className="bingo-cell">
+                    {number}
                   </div>
                 ))}
-                <div className="bingo-card-id">Card token: {token}</div>
               </div>
             ))}
+            <div className="bingo-card-id">Card token: {card.token}</div>
+          </div>
+      ))}
           </div>
     </div>
   );
